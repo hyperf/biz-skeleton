c7c45a2b489ce5b6b62d90f4b1475052f33898ba
李铭昕 <715557344@qq.com>
2019-05-28 15:02:27 +0800

    Added install cmd.

---
+++ installer/config.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://hyperf.io
+ * @document https://doc.hyperf.io
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf-cloud/hyperf/blob/master/LICENSE
+ */
+
+return [
+    'packages' => [
+        'hyperf/json-rpc' => [
+            'version' => 'dev-master',
+        ],
+        'hyperf/rpc' => [
+            'version' => 'dev-master',
+        ],
+        'hyperf/rpc-client' => [
+            'version' => 'dev-master',
+        ],
+        'hyperf/rpc-server' => [
+            'version' => 'dev-master',
+        ],
+        'hyperf/grpc-client' => [
+            'version' => 'dev-master',
+        ],
+        'hyperf/grpc-server' => [
+            'version' => 'dev-master',
+        ],
+    ],
+    'require-dev' => [
+    ],
+    'questions' => [
+        'rpc' => [
+            'question' => 'Which rpc component do you want to use?',
+            'default' => 'n',
+            'required' => false,
+            'custom-package' => true,
+            'options' => [
+                1 => [
+                    'name' => 'json-rpc',
+                    'packages' => [
+                        'hyperf/json-rpc',
+                        'hyperf/rpc',
+                        'hyperf/rpc-client',
+                        'hyperf/rpc-server',
+                    ],
+                    'resources' => [
+                    ],
+                ],
+                2 => [
+                    'name' => 'grpc',
+                    'packages' => [
+                        'hyperf/grpc-client',
+                        'hyperf/grpc-server',
+                    ],
+                    'resources' => [
+                    ],
+                ],
+            ],
+        ],
+    ],
+];
