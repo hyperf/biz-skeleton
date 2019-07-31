73b69f4d5b6a12f6a8fddd696fd6837a6090bf79
李铭昕 <715557344@qq.com>
2019-07-31 10:38:55 +0800

    Update databases.php

--- config/autoload/databases.php
+++ config/autoload/databases.php
@@ -18,8 +18,8 @@ return [
         'port' => env('DB_PORT', 3306),
         'username' => env('DB_USERNAME', 'root'),
         'password' => env('DB_PASSWORD', ''),
-        'charset' => env('DB_CHARSET', 'utf8'),
-        'collation' => env('DB_COLLATION', 'utf8_unicode_ci'),
+        'charset' => env('DB_CHARSET', 'utf8mb4'),
+        'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
         'prefix' => env('DB_PREFIX', ''),
         'pool' => [
             'min_connections' => 1,
