0aff7b1ca269cd9427b603444573a564ffac3480
李铭昕 <715557344@qq.com>
2020-03-03 10:32:22 +0800

    Update routes.php

--- config/routes.php
+++ config/routes.php
@@ -12,4 +12,4 @@ declare(strict_types=1);
 
 use Hyperf\HttpServer\Router\Router;
 
-Router::addRoute(['GET', 'POST', 'HEAD'], '/', 'App\Controller\IndexController@index');
+Router::addRoute(['GET', 'POST', 'HEAD'], '/', 'App\Controller\IndexController::index');
