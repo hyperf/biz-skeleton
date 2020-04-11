bd29bcd34c25bb9deb0f85176adeb990172f8e02
李铭昕 <715557344@qq.com>
2020-04-11 16:23:38 +0800

    Format code.

--- app/Kernel/Http/Response.php
+++ app/Kernel/Http/Response.php
@@ -9,7 +9,6 @@ declare(strict_types=1);
  * @contact  group@hyperf.io
  * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
-
 namespace App\Kernel\Http;
 
 use Hyperf\HttpMessage\Cookie\Cookie;
--- test/bootstrap.php
+++ test/bootstrap.php
@@ -9,7 +9,6 @@ declare(strict_types=1);
  * @contact  group@hyperf.io
  * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
-
 error_reporting(E_ALL);
 date_default_timezone_set('Asia/Shanghai');
 
