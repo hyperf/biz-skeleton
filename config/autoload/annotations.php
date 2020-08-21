68b017eacd64483e507b423fb02da692685f42ce
李铭昕 <715557344@qq.com>
2020-08-21 10:57:46 +0800

    Format code.

--- config/autoload/annotations.php
+++ config/autoload/annotations.php
@@ -5,12 +5,10 @@ declare(strict_types=1);
  * This file is part of Hyperf.
  *
  * @link     https://www.hyperf.io
- * @document https://doc.hyperf.io
+ * @document https://hyperf.wiki
  * @contact  group@hyperf.io
  * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
-use Hyperf\Utils\Coroutine;
-
 return [
     'scan' => [
         'paths' => [
@@ -20,7 +18,8 @@ return [
             'mixin',
         ],
         'class_map' => [
-            // Coroutine::class => BASE_PATH . '/app/Kernel/ClassMap/Coroutine.php',
+            // Hyperf\Utils\Coroutine::class => BASE_PATH . '/app/Kernel/ClassMap/Coroutine.php',
+            Hyperf\Di\Resolver\ResolverDispatcher::class => BASE_PATH . '/app/Kernel/ClassMap/ResolverDispatcher.php',
         ],
     ],
 ];
