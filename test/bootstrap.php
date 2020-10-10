6b5b81517d6e87d5060785b3ee1b171a6d22d170
李铭昕 <715557344@qq.com>
2020-10-10 14:05:58 +0800

    Update bootstrap.php

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -15,9 +15,9 @@ date_default_timezone_set('Asia/Shanghai');
 ! defined('BASE_PATH') && define('BASE_PATH', dirname(__DIR__, 1));
 ! defined('SWOOLE_HOOK_FLAGS') && define('SWOOLE_HOOK_FLAGS', SWOOLE_HOOK_ALL);
 
-Swoole\Runtime::enableCoroutine(true);
-
 require BASE_PATH . '/vendor/autoload.php';
+
+Swoole\Runtime::enableCoroutine(true, swoole_hook_flags());
 
 Hyperf\Di\ClassLoader::init();
 
