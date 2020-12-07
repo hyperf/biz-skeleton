41e76a6a9f39c3b6e2f1d3d2ac3db562869ab2ad
李铭昕 <715557344@qq.com>
2020-12-07 16:39:44 +0800

    Update `max_connections` to `32` for db and redis.

--- config/autoload/databases.php
+++ config/autoload/databases.php
@@ -22,7 +22,7 @@ return [
         'prefix' => env('DB_PREFIX', ''),
         'pool' => [
             'min_connections' => 1,
-            'max_connections' => 10,
+            'max_connections' => 32,
             'connect_timeout' => 10.0,
             'wait_timeout' => 3.0,
             'heartbeat' => -1,
--- config/autoload/redis.php
+++ config/autoload/redis.php
@@ -17,7 +17,7 @@ return [
         'db' => (int) env('REDIS_DB', 0),
         'pool' => [
             'min_connections' => 1,
-            'max_connections' => 10,
+            'max_connections' => 32,
             'connect_timeout' => 10.0,
             'wait_timeout' => 3.0,
             'heartbeat' => -1,
