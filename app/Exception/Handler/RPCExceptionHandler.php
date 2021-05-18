fe40e34900fa84d86eec21578fa50959ef6ce386
李铭昕 <715557344@qq.com>
2021-05-18 09:33:34 +0800

    Create RPCExceptionHandler.php

---
+++ app/Exception/Handler/RPCExceptionHandler.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+namespace App\Exception\Handler;
+
+use App\Exception\BusinessException;
+use Hyperf\Contract\StdoutLoggerInterface;
+use Hyperf\ExceptionHandler\ExceptionHandler;
+use Hyperf\ExceptionHandler\Formatter\FormatterInterface;
+use Psr\Http\Message\ResponseInterface;
+use Throwable;
+
+class RPCExceptionHandler extends ExceptionHandler
+{
+    /**
+     * @var StdoutLoggerInterface
+     */
+    protected $logger;
+
+    /**
+     * @var FormatterInterface
+     */
+    protected $formatter;
+
+    public function __construct(StdoutLoggerInterface $logger, FormatterInterface $formatter)
+    {
+        $this->logger = $logger;
+        $this->formatter = $formatter;
+    }
+
+    public function handle(Throwable $throwable, ResponseInterface $response)
+    {
+        if ($throwable instanceof BusinessException) {
+            $this->logger->warning($this->formatter->format($throwable));
+        } else {
+            $this->logger->error($this->formatter->format($throwable));
+        }
+
+        return $response;
+    }
+
+    public function isValid(Throwable $throwable): bool
+    {
+        return true;
+    }
+}
