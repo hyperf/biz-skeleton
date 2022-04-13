0297d321ab761fe90daa700f211f2e638cb942e0
李铭昕 <715557344@qq.com>
2022-04-13 18:54:07 +0800

    Resume exit coordinator after command executed.

--- config/container.php
+++ config/container.php
@@ -15,7 +15,4 @@ use Hyperf\Utils\ApplicationContext;
 
 $container = new Container((new DefinitionSourceFactory(true))());
 
-if (! $container instanceof \Psr\Container\ContainerInterface) {
-    throw new RuntimeException('The dependency injection container is invalid.');
-}
 return ApplicationContext::setContainer($container);
