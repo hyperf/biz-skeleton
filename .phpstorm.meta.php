e3491b862c71127ac42a0779ba80f3867b2dbbe0
李铭昕 <715557344@qq.com>
2022-06-10 17:18:48 +0800

    Support vscode

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -2,8 +2,8 @@
 
 namespace PHPSTORM_META {
     // Reflect
-    override(\Psr\Container\ContainerInterface::get(0), map('@'));
-    override(\Hyperf\Context\Context::get(0), map('@'));
-    override(\make(0), map('@'));
-    override(\di(0), map('@'));
+    override(\Psr\Container\ContainerInterface::get(0), map(['' => '@']));
+    override(\Hyperf\Context\Context::get(0), map(['' => '@']));
+    override(\make(0), map(['' => '@']));
+    override(\di(0), map(['' => '@']));
 }
