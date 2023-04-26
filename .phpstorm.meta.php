7671555b0808012902dfc280a985d4fba89bf8ae
李铭昕 <715557344@qq.com>
2023-04-26 12:27:23 +0800

    Update .phpstorm.meta.php

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -4,6 +4,9 @@ namespace PHPSTORM_META {
     // Reflect
     override(\Psr\Container\ContainerInterface::get(0), map(['' => '@']));
     override(\Hyperf\Context\Context::get(0), map(['' => '@']));
-    override(\make(0), map(['' => '@']));
+    override(\Hyperf\Support\make(0), map(['' => '@']));
+    override(\Hyperf\Support\optional(0), type(0));
+    override(\Hyperf\Tappable\tap(0), type(0));
+
     override(\di(0), map(['' => '@']));
 }
